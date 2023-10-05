d1b23a16fed71d8275e8c8e1a636971691a7e6a9
Alberto Acerbis <alberto.acerbis@gmail.com>
2023-10-05 16:49:40 +0200

    Complete consumers

---
+++ src/BrewUp.Purchases/BrewUp.Modules.Purchases.ReadModel/Entities/PurchaseOrder.cs
@@ -0,0 +1,30 @@
+﻿using BrewUp.Modules.Purchases.SharedKernel.DomainIds;
+using BrewUp.Modules.Purchases.SharedKernel.Dtos;
+using BrewUp.Shared.Abstracts;
+using BrewUp.Shared.DomainIds;
+using BrewUp.Shared.Dtos;
+
+namespace BrewUp.Modules.Purchases.ReadModel.Entities;
+
+public class PurchaseOrder : EntityBase
+{
+	public DateTime Date { get; private set; }
+	public IEnumerable<OrderLine> Lines { get; private set; }
+	public SupplierId SupplierId { get; private set; }
+	public Status Status { get; set; }
+
+	public static PurchaseOrder Create(PurchaseOrderId purchaseOrderId, DateTime date, IEnumerable<OrderLine> lines,
+		SupplierId supplierId)
+	{
+		return new PurchaseOrder(purchaseOrderId, date, lines, supplierId);
+	}
+
+	private PurchaseOrder(PurchaseOrderId purchaseOrderId, DateTime date, IEnumerable<OrderLine> lines,
+		SupplierId supplierId)
+	{
+		Date = date;
+		Lines = lines;
+		SupplierId = supplierId;
+		Status = Status.Created;
+	}
+}
---
+++ src/BrewUp/Modules/InfrastructureModule.cs
@@ -0,0 +1,15 @@
+﻿namespace BrewUp.Modules;
+
+public class InfrastructureModule : IModule
+{
+    public bool IsEnabled => true;
+    public int Order => 99;
+    
+    public IServiceCollection RegisterModule(WebApplicationBuilder builder)
+    {
+       
+        return builder.Services;
+    }
+
+    public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints) => endpoints;
+}
