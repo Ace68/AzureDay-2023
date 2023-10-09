45dd0c30d566a1b944249bb75fbbe456f4730191
Alberto Acerbis <alberto.acerbis@gmail.com>
2023-10-09 14:54:56 +0200

    Hide Warehouses

--- src/BrewUp/Modules/WarehousesModule.cs
+++ src/BrewUp/Modules/WarehousesModule.cs
@@ -4,7 +4,7 @@ namespace BrewUp.Modules;
 
 public class WarehousesModule : IModule
 {
-	public bool IsEnabled => true;
+	public bool IsEnabled => false;
 	public int Order => 0;
 
 	public IServiceCollection RegisterModule(WebApplicationBuilder builder)
