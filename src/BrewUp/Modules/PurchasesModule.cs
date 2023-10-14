2c4b4967f40c7433a90c231133799ff0d60c7bdf
Alberto Acerbis <alberto.acerbis@gmail.com>
2023-10-14 14:51:14 +0200

    dsadsad

--- src/BrewUp/Modules/PurchasesModule.cs
+++ src/BrewUp/Modules/PurchasesModule.cs
@@ -5,36 +5,36 @@ namespace BrewUp.Modules;
 
 public class PurchasesModule : IModule
 {
-	public bool IsEnabled => true;
-	public int Order => 20;
+    public bool IsEnabled => false;
+    public int Order => 20;
 
-	public IServiceCollection RegisterModule(WebApplicationBuilder builder)
-	{
-		builder.Services.AddPurchases();
+    public IServiceCollection RegisterModule(WebApplicationBuilder builder)
+    {
+        builder.Services.AddPurchases();
 
-		return builder.Services;
-	}
+        return builder.Services;
+    }
 
-	public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
-	{
-		var group = endpoints.MapGroup("/v1/Purchases/")
-			.WithTags("Purchases");
+    public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
+    {
+        var group = endpoints.MapGroup("/v1/Purchases/")
+            .WithTags("Purchases");
 
-		group.MapPost("/Order", PurchasesEndpoints.HandleCreateOrder)
-			.Produces(StatusCodes.Status400BadRequest)
-			.Produces(StatusCodes.Status201Created)
-			.WithName("CreateOrder");
+        group.MapPost("/Order", PurchasesEndpoints.HandleCreateOrder)
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status201Created)
+            .WithName("CreateOrder");
 
-		group.MapPut("/Order/{id}/complete", PurchasesEndpoints.HandleSetOrderStatusToComplete)
-			.Produces(StatusCodes.Status400BadRequest)
-			.Produces(StatusCodes.Status200OK)
-			.WithName("SetStatusToComplete");
+        group.MapPut("/Order/{id}/complete", PurchasesEndpoints.HandleSetOrderStatusToComplete)
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status200OK)
+            .WithName("SetStatusToComplete");
 
-		group.MapGet("/Order", PurchasesEndpoints.HandleGetPurchasesOrders)
-			.Produces(StatusCodes.Status400BadRequest)
-			.Produces(StatusCodes.Status201Created)
-			.WithName("GetOrders");
+        group.MapGet("/Order", PurchasesEndpoints.HandleGetPurchasesOrders)
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status201Created)
+            .WithName("GetOrders");
 
-		return endpoints;
-	}
+        return endpoints;
+    }
 }
